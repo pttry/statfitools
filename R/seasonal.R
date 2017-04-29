8bd68f3a71f68524ce8c756b4da2259c222fe8a9
Janne Huovari <janne.huovari@ptt.fi>
2017-04-29 22:11:20 +0300

    Outlier default for seasonal_adj

--- R/seasonal.R
+++ R/seasonal.R
@@ -8,9 +8,12 @@
 #'
 #' @param x a numeric vector to seasonal adjust.
 #' @param time a date vector.
-#' @param ... parameters passed to \code{\link[seasonal]{seas}}.
 #' @param series a series to return. "sa" for seasonal adjusted and "trend"
 #'        for trend.
+#' @param outlier.types an outlier parameter passed to
+#'        \code{\link[seasonal]{seas}}. Default "ao", only additive outliers
+#'        differs from \code{seas} default.
+#' @param ... parameters passed to \code{\link[seasonal]{seas}}.
 #'
 #' @export
 #' @import dplyr
@@ -20,12 +23,13 @@
 #'
 #' y <- sa_series(x = org$values, time = org$time)
 
-seasonal_adj <- function(x, time, ..., series = "sa"){
+seasonal_adj <- function(x, time, series = "sa", outlier.types = "ao", ...){
   freq <- 1/lubridate:: time_length(
     lubridate::interval(time[1], time[2]), unit = "year")
+  freq <- round(freq)
   x_ts <- ts(x, lubridate::year(time[1]), frequency = freq)
 
-  y <- try(seasonal::seas(x_ts, ...), silent = TRUE)
+  y <- try(seasonal::seas(x_ts, ..., outlier.types = outlier.types), silent = TRUE)
 
   #If fails..
   if (inherits(y, "try-error")){
@@ -46,14 +50,15 @@ seasonal_adj <- function(x, time, ..., series = "sa"){
 
 
 #' @describeIn seasonal_adj seasonal adjusted series
-
+#' @export
+#'
 sa_series <- function(x, time, ...){
   y <- seasonal_adj(x, time, ..., series = "sa")
   y
 }
 
 #' @describeIn seasonal_adj trend of seasonal adjusted series
-#'
+#' @export
 
 trend_series <- function(x, time, ...){
   y <- seasonal_adj(x, time, ..., series = "trend")
