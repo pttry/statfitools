67c16e99b4a7dcaae4c8a6e30efd12e733def766
Janne Huovari <janne.huovari@ptt.fi>
2017-05-05 22:02:11 +0300

    Fix na

--- R/seasonal.R
+++ R/seasonal.R
@@ -24,12 +24,14 @@
 #' y <- sa_series(x = org$values, time = org$time)
 
 seasonal_adj <- function(x, time, series = "sa", outlier.types = "ao", ...){
+
   freq <- 1/lubridate:: time_length(
     lubridate::interval(time[1], time[2]), unit = "year")
   freq <- round(freq)
   x_ts <- ts(x, lubridate::year(time[1]), frequency = freq)
 
-  y <- try(seasonal::seas(x_ts, ..., outlier.types = outlier.types), silent = TRUE)
+  y <- try(seasonal::seas(x_ts, ..., outlier.types = outlier.types,
+                          na.action = na.exclude), silent = TRUE)
 
   #If fails..
   if (inherits(y, "try-error")){
