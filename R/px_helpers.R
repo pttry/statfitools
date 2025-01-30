d0c4b0dd1d58caf9c200819965c87206ee5fab74
Janne Huovari <janne.huovari@gmail.com>
2025-01-30 12:47:01 +0200

    Change options for codes2name to_name default is now TRUE which does same as previous default, also FALSE possible which just pass data.

--- R/px_helpers.R
+++ R/px_helpers.R
@@ -3,7 +3,9 @@
 #' @param .data A data.frame or similar.
 #' @param codes_names A named (column codes) list of named (codes) vectors (names).
 #'        For example, from [px_code_name()].
-#' @param to_name A vector of column names to be translated to names. Default is the names from `codes_names`.
+#' @param to_name A vector of column names to be translated to names.
+#'                Default (TRUE) is the names from `codes_names`.
+#'                FALSE or NULL just pass `.data`.
 #' @param name_suffix Suffix to add to name columns. Default is `"_name"`.
 #' @param code_suffix Suffix to add to code columns. Default is `"_code"`.
 #'
@@ -18,10 +20,16 @@
 #'            b = c("b1" = "other", "b2" = "something"))
 #' codes2names(x, cn)
 #' codes2names(x, cn, to_name = "a", name_suffix = "_label", code_suffix = "_identifier")
-codes2names <- function(.data, codes_names, to_name = names(codes_names),
+codes2names <- function(.data, codes_names, to_name = TRUE,
                         name_suffix = "_name", code_suffix = "_code") {
 
   if (is.null(to_name)) return(.data)
+
+  if (is.character(to_name)) {
+    to_name <- to_name
+  } else if (to_name) {
+    to_name <- names(codes_names)
+  } else if (!to_name) return(.data)
 
   if (name_suffix == "") stop("name_suffix can not be empty")
 
